e34a540f9fd07045d4610d45f409fba87ccf8021
Vaibhav_Shukla <vaibhavshukla2704@gmail.com>
2023-05-11 15:44:11 +0530

    home page is completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import Logo from '../images/logo.png';
 import { FaLinkedin } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
+import { FaFacebookF } from 'react-icons/fa';
 import { AiOutlineTwitter } from 'react-icons/ai';
 import { AiFillInstagram } from 'react-icons/ai';
 
@@ -30,7 +30,7 @@ const Footer = () => {
               target="_blank"
               rel="noreferrer noopener"
             >
-              <FaFacebook />
+              <FaFacebookF />
             </a>
             <a
               href="https://www.twitter.com"
