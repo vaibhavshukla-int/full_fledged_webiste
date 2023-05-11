2447414687e018b5a56db7b5cf23c9c8927585e1
Vaibhav_Shukla <vaibhavshukla2704@gmail.com>
2023-05-11 19:45:10 +0530

    plans ssection is completed

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,7 +1,44 @@
+import OtherPageHeader from '../../components/OtherPageHeader';
+import PlansHeaderImage from '../../images/header_bg_4.jpg';
+import Cards from '../../UI/Cards';
+import { plans } from '../../data';
 import './plans.css';
 
 const Plans = () => {
-  return <h1>Plans</h1>;
+  return (
+    <>
+      <OtherPageHeader title="Membership Plans" image={PlansHeaderImage}>
+        Cupidatat eiusmod eiusmod ullamco occaecat mollit magna aute
+        sit.Pariatur ea esse esse ad sit amet.
+      </OtherPageHeader>
+
+      <section className="plans">
+        <div className="container plans__container">
+          {plans.map((plan) => {
+            const { id, name, desc, price, features } = plan;
+            return (
+              <Cards key={id} className="plan">
+                <h3>{name}</h3>
+                <small>{desc}</small>
+                <h1>{`$${price}`}</h1>
+                <h2>/mo</h2>
+                <h4>Features</h4>
+                {features.map((fea, index) => {
+                  const { feature, available } = fea;
+                  return (
+                    <p key={index} className={!available ? 'disabled' : ''}>
+                      {feature}
+                    </p>
+                  );
+                })}
+                <button className="btn lg">Choose Plan</button>
+              </Cards>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Plans;
